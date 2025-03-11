41eb6ea160964051c87d9c16122acebb4c489dd3
william <williamwambugu663@gmail.com>
2025-03-11 17:33:22 +0300

    DOM

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+//removing "main" element in the id
+document.getElementById("main").remove();
+
+//creating a newHeader and giving it an id and text
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "William Wambugu is the champion";
+
+//displaying it using append...
+document.body.appendChild(newHeader);
